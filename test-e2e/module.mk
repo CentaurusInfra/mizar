907b439f3a5a2c9d6dfa688165a213506d216736
Sherif Abdelwahab <51762816+zasherif@users.noreply.github.com>
2020-03-31 16:23:42 -0700

    Add a module file to run new tests

---
+++ test-e2e/module.mk
@@ -0,0 +1,21 @@
+
+e2efunctest::
+ifdef nocleanup
+	echo "Retaining test environment. Please delete containers after debugging"
+	export NOCLEANUP=1; sudo python3 -W ignore -m unittest discover test-e2e/ -v
+else
+	sudo python3 -W ignore -m unittest discover test-e2e -v
+endif
+
+.PHONY: run_e2efunc_test
+run_e2efunc_test:
+ifdef nocleanup
+	echo "Retaining test environment. Please delete containers after debugging"
+	export NOCLEANUP=1; sudo python3 -W ignore -m unittest test-e2e.${TEST}
+else
+	sudo python3 -W ignore -m unittest test-e2e.${TEST}
+endif
+
+clean::
+	rm -rf test-e2e/__pycache__
+	rm -rf test-e2e/*.pyc
