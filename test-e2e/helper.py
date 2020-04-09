3324e049c35386edfc2af5fb8eba2a4a4efe8367
clu2xlu <62575920+clu2xlu@users.noreply.github.com>
2020-04-09 13:37:18 -0700

    add tcp udp http tests (#134)
    * add tcp udp http tests
    
    * Fixes from incorrect merge resolution
    
    * Annotate builtin endpoints for scaled ep services
    
    * Fix for NoneType droplet object on-provisioned
    
    * Scaled endpoint delete implementation
    
    * Removing unused constants for delete work
    
    * adding helper function to run tcp http udp tests)
    
    Co-authored-by: Sherif Abdelwahab <51762816+zasherif@users.noreply.github.com>
    Co-authored-by: Phu Tran <22720475+phudtran@users.noreply.github.com>

---
+++ test-e2e/helper.py
@@ -0,0 +1,67 @@
+import unittest
+import os
+from common import *
+from k8s import *
+
+def do_common_tests(test, ep1, ep2):
+    do_ping_test(test, ep1, ep2)
+    do_http_test(test, ep1, ep2)
+    do_tcp_test(test, ep1, ep2)
+    do_udp_test(test, ep1, ep2)
+
+def do_ping_test(test, ep1, ep2, both_ways=True):
+    logger.info("-------Test: PING test-------")
+    if ep1.do_ping(ep2.ip):
+        logger.info("Ping test from {} to {} was Successfull".format(ep1.name, ep2.name))
+    else:
+        logger.info("Ping test from {} to {} FAILED".format(ep1.name, ep2.name))
+    if both_ways:
+        logger.info("Test: {} can ping {}".format(ep2.ip, ep1.ip))
+        if ep2.do_ping(ep1.ip):
+            logger.info("Ping test from {} to {} was Successfull".format(ep2.name, ep1.name))
+        else:
+            logger.info("Ping test from {} to {} was Successfull".format(ep2.name, ep1.name))
+
+def do_http_test(test, ep1, ep2):
+    logger.info("-------Test: HTTP test-------")
+    #ep1.do_httpd()
+    #ep2.do_httpd()
+
+    if ep2.do_curl("http://{}:8000 -Ss -m 1".format(ep1.ip)):
+        logger.info("Http test from {} to {} was Successfull".format(ep2.name, ep1.name))
+    else:
+        logger.info("Http test from {} to {} FAILED".format(ep2.name, ep1.name))
+
+    if ep1.do_curl("http://{}:8000 -Ss -m 1".format(ep2.ip)):
+        logger.info("Http test from {} to {} was Successfull".format(ep1.name, ep2.name))
+    else:
+        logger.info("Http test from {} to {} FAILED".format(ep1.name, ep2.name))
+
+def do_tcp_test(test, ep1, ep2):
+    logger.info("-------Test: TCP test-------")
+    #ep1.do_tcp_serve()
+    #ep2.do_tcp_serve()
+
+    if ep2.do_tcp_client(ep1.ip):
+        logger.info("TCP test from {} to {} was Successfull".format(ep1.name, ep2.name))
+    else:
+        logger.info("TCP test from {} to {} FAILED".format(ep1.name, ep2.name))
+
+    if ep1.do_tcp_client(ep2.ip):
+        logger.info("TCP test from {} to {} was Successfull".format(ep2.name, ep1.name))
+    else:
+        logger.info("TCP test from {} to {} FAILED".format(ep2.name, ep1.name))
+
+def do_udp_test(test, ep1, ep2):
+    logger.info("-------Test: UDP test-------")
+    #ep1.do_udp_serve()
+    #ep2.do_udp_serve()
+
+    if ep2.do_udp_client(ep1.ip):
+        logger.info("UDP test from {} to {} was Successfull".format(ep1.name, ep2.name))
+    else:
+        logger.info("UDP test from {} to {} FAILED".format(ep1.name, ep2.name))
+    if ep1.do_udp_client(ep2.ip):
+        logger.info("UDP test from {} to {} was Successfull".format(ep2.name, ep1.name))
+    else:
+        logger.info("UDP test from {} to {} FAILED".format(ep2.name, ep1.name))
