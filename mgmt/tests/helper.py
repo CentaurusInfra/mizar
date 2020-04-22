e80ee1f19467eba965dc5e9f8c0ea63dd48699e6
Phu Tran <22720475+phudtran@users.noreply.github.com>
2020-04-21 17:17:55 -0700

    Added basic tests for maglev hash table

---
+++ mgmt/tests/helper.py
@@ -0,0 +1,15 @@
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def do_add_remove_maglev(test, table, add, remove, exp_table):
+    if add is not None:
+        logger.info("Adding {} to the table.".format(add))
+        table.add(add)
+    if remove is not None:
+        logger.info("Removing {} from the table.".format(remove))
+        table.remove(remove)
+    act_table = table.get_table()
+    test.assertEqual(exp_table, act_table)
