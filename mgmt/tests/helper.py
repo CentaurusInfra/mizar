80d40ce81ea46ac5734a43b5006d74a1ef3ed165
Phu Tran <22720475+phudtran@users.noreply.github.com>
2020-04-22 18:49:04 -0700

    Added a table that tracks lookup table alterations

--- mgmt/tests/helper.py
+++ mgmt/tests/helper.py
@@ -4,7 +4,8 @@ logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def do_add_remove_maglev(test, table, add, remove, exp_table):
+def do_add_remove_maglev(test, table, add, remove, exp_table, exp_prev_ele_map):
+    logger.info("===Add/Remove Maglev test===")
     if add:
         logger.info("Adding {} to the table.".format(add))
         table.add(add)
@@ -12,4 +13,8 @@ def do_add_remove_maglev(test, table, add, remove, exp_table):
         logger.info("Removing {} from the table.".format(remove))
         table.remove(remove)
     act_table = table.get_table()
+    act_prev_ele_map = table.get_prev_elements_map()
     test.assertEqual(exp_table, act_table)
+    test.assertEqual(act_prev_ele_map, exp_prev_ele_map)
+    logger.info("Number of elements replaced {}".format(table.elements_replaced))
+    logger.info("Number of elements replacing {}".format(table.elements_replacing))
