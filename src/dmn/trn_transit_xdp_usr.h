838fa948fef0e591db6066b140f1f22c9bef99ee
Shixiong Qi <qishixiong19931009@gmail.com>
2020-07-25 15:51:42 -0700

    ADD int metrics_table_ref_fd and struct bpf_map* metrics_table_ref

--- src/dmn/trn_transit_xdp_usr.h
+++ src/dmn/trn_transit_xdp_usr.h
@@ -65,6 +65,7 @@ struct ebpf_prog_stage_t {
 	int rev_flow_mod_cache_ref_fd;
 	int ep_flow_host_cache_ref_fd;
 	int ep_host_cache_ref_fd;
+	int metrics_table_ref_fd;
 
 	struct bpf_map *networks_map_ref;
 	struct bpf_map *vpc_map_ref;
@@ -77,6 +78,7 @@ struct ebpf_prog_stage_t {
 	struct bpf_map *rev_flow_mod_cache_ref;
 	struct bpf_map *ep_flow_host_cache_ref;
 	struct bpf_map *ep_host_cache_ref;
+	struct bpf_map *metrics_table_ref;
 };
 
 struct user_metadata_t {
