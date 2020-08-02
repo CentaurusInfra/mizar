74557f23f5889d12f727e06657ca3537f7ecc075
Ubuntu <ubuntu@ip-172-31-18-11.us-west-1.compute.internal>
2020-08-02 17:34:54 +0000

    Move functions to arktos_up_adaptor.sh

--- install/environment_adaptors/k8s_kind_adaptor.sh
+++ install/environment_adaptors/k8s_kind_adaptor.sh
@@ -4,7 +4,6 @@
 # Copyright (c) 2020 The Authors.
 
 # Authors: Hong Chang        <@Hong-Chang>
-#          Sherif Abdelwahab <@zasherif>
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
