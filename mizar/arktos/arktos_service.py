877eb2e66a5b6cfa39b2c262178dde624f8d8b77
Phu Tran <22720475+phudtran@users.noreply.github.com>
2020-09-14 18:04:47 -0700

    Append namespace and tenant to ep name when delete pod, service

--- mizar/arktos/arktos_service.py
+++ mizar/arktos/arktos_service.py
@@ -149,14 +149,16 @@ class ArktosService(BuiltinsServiceServicer):
         logger.info(
             "Deleting pod from Network Controller {}".format(request.name))
         param = HandlerParam()
-        param.name = request.name
+        param.name = "{}-{}-{}".format(request.name,
+                                       request.namespace, request.tenant)
         return run_arktos_workflow(wffactory().k8sPodDelete(param=param))
 
     def DeleteService(self, request, context):
         logger.info(
             "Deleting servoce from Network Controller {}".format(request.name))
         param = HandlerParam()
-        param.name = request.name
+        param.name = "{}-{}-{}".format(request.name,
+                                       request.namespace, request.tenant)
         return run_arktos_workflow(wffactory().k8sServiceDelete(param=param))
 
 
