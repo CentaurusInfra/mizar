f419b38ceaacf96638af3c4998dfe9000c467af1
Hongwei Chen <hong.chen@futurewei.com>
2020-12-21 10:54:58 -0800

    trivial: format change

--- src/include/shared_map_names.h
+++ src/include/shared_map_names.h
@@ -3,7 +3,7 @@
 // global pinned map file paths
 static const char *eg_vsip_enforce_map_path	= "/sys/fs/bpf/eg_vsip_enforce_map";
 static const char *eg_vsip_prim_map_path	= "/sys/fs/bpf/eg_vsip_prim_map";
-static const char *eg_vsip_ppo_map_path		= "/sys/fs/bpf/eg_vsip_ppo_map";
+static const char *eg_vsip_ppo_map_path 	= "/sys/fs/bpf/eg_vsip_ppo_map";
 static const char *eg_vsip_supp_map_path	= "/sys/fs/bpf/eg_vsip_supp_map";
 static const char *eg_vsip_except_map_path	= "/sys/fs/bpf/eg_vsip_except_map";
 static const char *ing_vsip_enforce_map_path	= "/sys/fs/bpf/ing_vsip_enforce_map";
