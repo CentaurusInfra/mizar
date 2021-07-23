98c43e5d806b1062f3436b12ce908c05761b17c3
Hong-Chang <hchang@futurewei.com>
2021-07-23 08:07:06 +0000

    Remove debug info

--- mizar/module.mk
+++ mizar/module.mk
@@ -4,19 +4,11 @@ submodules := tests
 -include $(patsubst %, $(module)/%/module.mk, $(submodules))
 
 GRPC_FLAGS := -I mizar/proto/ --python_out=. --grpc_python_out=.
-
 PATH := $(PATH):/usr/local/go/bin:$(HOME)/go/bin
 
 .PHONY: proto
 proto:
 	python3 -m grpc_tools.protoc $(GRPC_FLAGS) mizar/proto/mizar/proto/*.proto
-	whereis protoc
-	whereis protoc-gen-go
-	printenv
-	echo /usr/bin/protoc
-	ls /usr/bin/protoc
-	echo $(HOME)/go/bin
-	ls $(HOME)/go/bin
 	protoc --go_out=. --go-grpc_out=. mizar/proto/mizar/proto/interface.proto
 
 clean::
