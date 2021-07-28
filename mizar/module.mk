20c0febeab19fac0023aa93c9f19b76f570cce9d
Hong-Chang <hchang@futurewei.com>
2021-07-28 20:11:51 +0000

    Fix make clean issue

--- mizar/module.mk
+++ mizar/module.mk
@@ -17,4 +17,4 @@ proto_go:
 clean::
 	rm -rf mizar/proto/__pycache__
 	find mizar/proto/ -name '*.py' -not -name '__init__.py' -delete
-	find cmd/mizarcni/main/ -name '*.pb.go' -delete
+	find cmd/mizarcni/app/ -name '*.pb.go' -delete
