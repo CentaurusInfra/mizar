93e25c7c0464c22432538014f52f193a2ba74033
Hong-Chang <hchang@futurewei.com>
2021-08-05 20:49:47 +0000

    Add more test to netutil_test.go

--- pkg/util/netutil/netutil_test.go
+++ pkg/util/netutil/netutil_test.go
@@ -22,21 +22,54 @@ import (
 
 	"centaurusinfra.io/mizar/pkg/util/executil"
 	"centaurusinfra.io/mizar/pkg/util/netutil"
+	"github.com/containernetworking/plugins/pkg/ip"
+	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/containernetworking/plugins/pkg/testutils"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 // The test cases in this package need sudo permission
 // It cannot be executed successfully in environments such as Visual Studio Code
-// Can be executed by command "sudo /usr/local/go/bin/go test /home/ubuntu/go/src/k8s.io/mizar/pkg/util/netutil/netutil_test.go"
+// Can be executed by example command "sudo /usr/local/go/bin/go test /home/ubuntu/go/src/k8s.io/mizar/pkg/util/netutil/netutil_test.go"
+// To execute all go test, example command is "sudo /usr/local/go/bin/go test /home/ubuntu/go/src/k8s.io/mizar/..."
+
+func Test_ActivateInterface(t *testing.T) {
+	Convey("Subject: netutil.ActivateInterface", t, func() {
+		ifName := "eth-894cad92"
+		var netNSNameToClear string
+
+		Convey("Given interface is up, get expected result", func() {
+			netNS, netNSName := createNetNS()
+			t.Log(netNSName)
+			netNSNameToClear = netNSName
+			_, err := ns.GetNS("/var/run/netns/" + netNSName)
+			So(err, ShouldBeNil)
+
+			hostVeth, _, err := ip.SetupVeth(ifName, 1500, netNS)
+			So(err, ShouldBeNil)
+
+			info, err := netutil.ActivateInterface(
+				hostVeth.Name,
+				netNSName,
+				ifName,
+				"8",
+				"20.0.0.81",
+				"20.0.0.1")
+			So(err, ShouldBeNil)
+			So(info, ShouldEqual, "Interface eth-894cad92 has already been UP.")
+		})
+
+		Reset(func() {
+			executil.Execute("ip", "link", "delete", ifName)
+			netutil.DeleteNetNS(netNSNameToClear)
+		})
+	})
+}
 
 func Test_DeleteNetNS(t *testing.T) {
 	Convey("Subject: netutil.DeleteNetNS", t, func() {
 		Convey("Given correct input, get expected result", func() {
-			netNS, _ := testutils.NewNS()
-			pathes := strings.Split(netNS.Path(), "/")
-			netNSName := pathes[len(pathes)-1]
-
+			_, netNSName := createNetNS()
 			_, exeResult, _ := executil.Execute("ip", "netns", "ls")
 			So(exeResult, ShouldContainSubstring, netNSName)
 
@@ -48,6 +81,51 @@ func Test_DeleteNetNS(t *testing.T) {
 
 		Convey("Given wrong input, no error thrown", func() {
 			So(func() { netutil.DeleteNetNS("Dummy Network Namespace") }, ShouldNotPanic)
+		})
+	})
+}
+
+func createNetNS() (ns.NetNS, string) {
+	netNS, _ := testutils.NewNS()
+	pathes := strings.Split(netNS.Path(), "/")
+	return netNS, pathes[len(pathes)-1]
+}
+
+func Test_ParseCIDR(t *testing.T) {
+	Convey("Subject: netutil.ParseCIDR", t, func() {
+		Convey("Given correct input, get expected result", func() {
+			ip, ipNet, err := netutil.ParseCIDR("20.0.0.8/16")
+			So(ip.String(), ShouldEqual, "20.0.0.8")
+			So(ipNet.String(), ShouldEqual, "20.0.0.0/16")
+			So(err, ShouldBeNil)
+		})
+
+		Convey("Given no mask, get expected result", func() {
+			ip, ipNet, err := netutil.ParseCIDR("20.0.0.8")
+			So(ip.String(), ShouldEqual, "20.0.0.8")
+			So(ipNet.String(), ShouldEqual, "20.0.0.8/32")
+			So(err, ShouldBeNil)
+		})
+
+		Convey("Given wrong input, get error", func() {
+			ip, ipNet, err := netutil.ParseCIDR("256.0.0.8/16")
+			So(ip, ShouldBeNil)
+			So(ipNet, ShouldBeNil)
+			So(err, ShouldNotBeNil)
+		})
+	})
+}
+
+func Test_ParseIP(t *testing.T) {
+	Convey("Subject: netutil.ParseIP", t, func() {
+		Convey("Given correct input, get expected result", func() {
+			ip := netutil.ParseIP("20.0.0.8")
+			So(ip.String(), ShouldEqual, "20.0.0.8")
+		})
+
+		Convey("Given wrong input, get nil", func() {
+			ip := netutil.ParseIP("20.0.0.8/16")
+			So(ip, ShouldBeNil)
 		})
 	})
 }
