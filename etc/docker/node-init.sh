cac2f5b65b362634681fba9f9642117bf03d6d0d
Click2cloud-alpha1 <82659464+Click2cloud-alpha1@users.noreply.github.com>
2021-08-18 05:50:22 +0530

    Fix for containerd restart while deploying the yaml (#524)

--- etc/docker/node-init.sh
+++ etc/docker/node-init.sh
@@ -63,12 +63,12 @@ if [[ "$parsedVersion" -lt "37" ]] ; then
 fi
 nsenter -t 1 -m -u -n -i mkdir -p /opt/cni/bin && \
 nsenter -t 1 -m -u -n -i mkdir -p /etc/cni/net.d && \
+nsenter -t 1 -m -u -n -i ln -snf /var/mizar/mizar/cni.py /opt/cni/bin/mizarcni && \
 nsenter -t 1 -m -u -n -i pip3 install --upgrade protobuf && \
 nsenter -t 1 -m -u -n -i pip3 install --ignore-installed /var/mizar/ && \
 nsenter -t 1 -m -u -n -i ln -snf /sys/fs/bpf /bpffs && \
 nsenter -t 1 -m -u -n -i ln -snf /var/mizar/build/bin /trn_bin && \
 nsenter -t 1 -m -u -n -i ln -snf /var/mizar/build/xdp /trn_xdp && \
-nsenter -t 1 -m -u -n -i ln -snf /var/mizar/etc/cni/10-mizarcni.conf /etc/cni/net.d/10-mizarcni.conf && \
-nsenter -t 1 -m -u -n -i ln -snf /var/mizar/mizar/cni.py /opt/cni/bin/mizarcni && \
+nsenter -t 1 -m -u -n -i cp -f /var/mizar/etc/cni/10-mizarcni.conf /etc/cni/net.d/10-mizarcni.conf && \
 nsenter -t 1 -m -u -n -i ln -snf /var/mizar/build/tests/mizarcni.config /etc/mizarcni.config && \
 echo "mizar-complete"
