e4e60ef999e9c2c2999c59539dc574fd6fde5c79
Jun Shao <jshao@futurewei.com>
2021-12-06 11:05:48 -0800

    Fixed the issue #573 to add v1.6.4 to goconvey in bootstrap.sh (#576)

--- bootstrap.sh
+++ bootstrap.sh
@@ -72,7 +72,7 @@ export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 sudo apt-get install -y protobuf-compiler libprotobuf-dev
 GO111MODULE="on" go get google.golang.org/protobuf/cmd/protoc-gen-go@v1.26
 GO111MODULE="on" go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
-GO111MODULE="on" go get github.com/smartystreets/goconvey
+GO111MODULE="on" go get github.com/smartystreets/goconvey@v1.6.4
 
 git submodule update --init --recursive
 
