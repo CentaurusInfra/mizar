1c0878043ab3faab443c0614bdf6944f2c0da56a
杨鹏 <707042815@qq.com>
2021-12-09 19:33:30 +0800

    update easy shell for ustc

--- install/make_image_daemon.sh
+++ install/make_image_daemon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=${1:-.}
 DOCKER_ACC=${2:-"yangpengzx"}
-VERSION=${3:-"0.92"}
+VERSION=${3:-"0.93"}
 
 docker image build -t $DOCKER_ACC/dropletd_ustc:$VERSION -f $DIR/etc/docker/daemon_ustc.Dockerfile $DIR
 docker image push $DOCKER_ACC/dropletd_ustc:$VERSION
