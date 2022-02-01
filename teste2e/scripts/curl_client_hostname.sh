dbf3dd34b3921865a860356b89fe16800bee01a9
Phu <22720475+phudtran@users.noreply.github.com>
2022-02-01 12:04:59 -0800

    curl timeout in test to 5 seconds

--- teste2e/scripts/curl_client_hostname.sh
+++ teste2e/scripts/curl_client_hostname.sh
@@ -3,7 +3,7 @@ TYPE=$2
 
 if [ "$TYPE" == "scaled" ]
 then
-    curl http://$IP:8000 -Ss -m 1
+    curl http://$IP:8000 -Ss -m 5
 else
-    curl http://$IP:7000 -Ss -m 1
+    curl http://$IP:7000 -Ss -m 5
 fi
