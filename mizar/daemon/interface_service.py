66ebe2baff8ba83f18144f372e3deeb2abf68743
Phu <22720475+phudtran@users.noreply.github.com>
2022-03-04 19:20:28 -0800

    Always get veth-index before get mac

--- mizar/daemon/interface_service.py
+++ mizar/daemon/interface_service.py
@@ -91,19 +91,20 @@ class InterfaceServer(InterfaceServiceServicer):
             try:
                 logger.info("Creating interface {}".format(veth_name))
                 self.iproute.link('add', ifname=veth_name,
-                                peer=veth_peer, kind='veth')
+                                  peer=veth_peer, kind='veth')
             except Exception as e:
                 if e.code == CONSTANTS.NETLINK_FILE_EXISTS_ERROR:
-                    logger.info("Veth already exists! Continuing")
+                    veth_index = get_iface_index(veth_name, self.iproute)
+                    logger.info(
+                        "Veth already exists! veth index is {} Continuing".format(veth_index))
                     pass
-
                 else:
                     logger.info(
                         "Unknown exception occured when creating veth {}".format(e))
-            veth_index = get_iface_index(veth_name, self.iproute)
         else:
             logger.info("Interface {} already exists!".format(veth_name))
 
+        veth_index = get_iface_index(veth_name, self.iproute)
         # Update the mac address with the interface address
         address = InterfaceAddress(
             version=interface.address.version,
@@ -189,7 +190,8 @@ class InterfaceServer(InterfaceServiceServicer):
         update the agent metadata and endpoint.
         """
         pod_name = get_pod_name(interface.interface_id.pod_id)
-        logger.info("Loading transit agent and configuring for pod {}".format(pod_name))
+        logger.info(
+            "Loading transit agent and configuring for pod {}".format(pod_name))
         self.rpc.load_transit_agent_xdp(interface)
 
         for bouncer in interface.bouncers:
