004ecf2e70457b00aa79ea6becf6660bf95e67c6
Yan Mo <andymo96@gmail.com>
2022-03-11 00:15:08 -0800

    added licence info

--- src/dmn/trn_bw_qos_monitor.c
+++ src/dmn/trn_bw_qos_monitor.c
@@ -1,3 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/**
+ * @file trn_agent_xdp_usr.c
+ * @author The Mizar Team
+ *
+ * @brief User space APIs to program transit agent
+ *
+ * @copyright Copyright (c) 2022 The Authors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
